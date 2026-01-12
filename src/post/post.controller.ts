4a39af214057a243ee2c5de10357cc427dcafd62
Rahmankhattak711 <rahmankhattak796@gmail.com>
2026-01-13 00:38:43 +0500

    feat : add in post update delete and get all post

--- src/post/post.controller.ts
+++ src/post/post.controller.ts
@@ -1,6 +1,16 @@
-import { Body, Controller, Post, Req, UseGuards } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Post,
+  Put,
+  Req,
+  UseGuards,
+} from '@nestjs/common';
 import { AuthGuard } from 'src/config/auth.guard';
-import { CreatePostDto } from './dto/create.post.dto';
+import { CreatePostDto, UpdatePostDto } from './dto/create.post.dto';
 import { PostService } from './post.service';
 
 @Controller('post')
@@ -11,5 +21,27 @@ export class PostController {
   @Post('create')
   async createPost(@Body() CreatePostDto: CreatePostDto, @Req() req) {
     return this.postService.create(CreatePostDto, req.user.id);
+  }
+
+  @UseGuards(AuthGuard)
+  @Put(':postId')
+  async updatePost(
+    @Body() UpdatePostDto: UpdatePostDto,
+    @Param('postId') postId: string,
+    @Req() req,
+  ) {
+    return this.postService.updatePostById(UpdatePostDto, req.user.id, postId);
+  }
+
+  @UseGuards(AuthGuard)
+  @Delete('delete/:postId')
+  async deletePost(@Param('postId') postId: string, @Req() req) {
+    return this.postService.deletePostById(req.user.id, postId);
+  }
+
+  @UseGuards(AuthGuard)
+  @Get('all')
+  async getAllPosts(@Req() req) {
+    return this.postService.getAllPosts();
   }
 }
--- src/post/post.service.ts
+++ src/post/post.service.ts
@@ -1,13 +1,16 @@
-import { Injectable } from '@nestjs/common';
+import {
+  Injectable,
+  NotFoundException,
+  UnauthorizedException,
+} from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
-import { CreatePostDto } from './dto/create.post.dto';
+import { CreatePostDto, UpdatePostDto } from './dto/create.post.dto';
 
 @Injectable()
 export class PostService {
   constructor(private readonly prisma: PrismaService) {}
 
   async create(dto: CreatePostDto, userID: string) {
-
     const newPost = await this.prisma.post.create({
       data: {
         title: dto.title,
@@ -20,6 +23,69 @@ export class PostService {
     return {
       message: 'Post created successfully',
       post: newPost,
+    };
+  }
+
+  async updatePostById(UpdatePostDto: UpdatePostDto, userId, postId: string) {
+    const post = await this.prisma.post.findUnique({
+      where: { id: postId },
+    });
+
+    if (!post) {
+      throw new NotFoundException('Post not found');
+    }
+
+    if (post.authorId !== userId) {
+      throw new UnauthorizedException(
+        'You are not authorized to update this post',
+      );
+    }
+
+    const updatePost = await this.prisma.post.update({
+      where: { id: postId },
+      data: {
+        title: UpdatePostDto.title,
+        content: UpdatePostDto.content,
+        published: UpdatePostDto.published ?? false,
+      },
+    });
+
+    return {
+      message: 'Post updated successfully',
+      post: updatePost,
+    };
+  }
+
+  async deletePostById(userId: string, postId: string) {
+    const post = await this.prisma.post.findUnique({
+      where: { id: postId },
+    });
+
+    if (!post) {
+      throw new NotFoundException('Post not found');
+    }
+
+    if (post.authorId !== userId) {
+      throw new UnauthorizedException(
+        'You are not authorized to delete this post',
+      );
+    }
+
+    const deleteMany = await this.prisma.post.delete({
+      where: { id: postId },
+    });
+
+    return {
+      message: 'Post deleted successfully',
+      post: deleteMany,
+    };
+  }
+
+  async getAllPosts() {
+    const posts = await this.prisma.post.findMany();
+    return {
+      message: 'Posts retrieved successfully',
+      posts: posts,
     };
   }
 }
