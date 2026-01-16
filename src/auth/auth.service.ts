b48e75623742df8fa53f7608ca3da70adea7e823
Rahmankhattak711 <rahmankhattak796@gmail.com>
2026-01-17 00:52:09 +0500

    feat : add user profile

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -95,4 +95,27 @@ export class AuthService {
       refreshToken,
     };
   }
+
+  async getProfile(userId: string) {
+    const user = await this.prisma.user.findUnique({
+      where: { id: userId },
+      select: {
+        id: true,
+        username: true,
+        email: true,
+        role: true,
+        createdAt: true,
+        updatedAt: true,
+      },
+    });
+
+    if (!user) {
+      throw new UnauthorizedException('User not found');
+    }
+
+    return {
+      message: 'User profile fetched successfully',
+      user,
+    };
+  }
 }
